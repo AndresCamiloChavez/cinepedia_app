43ba133130e6e11d023065dda56d0e2536f5fd7d
AndresChavezCZ <andres.cz.chavez@gmail.com>
2023-04-16 21:01:41 -0500

    Dio

---
+++ lib/infraestructure/moviedb_datasource.dart
@@ -0,0 +1,19 @@
+import 'package:cinepedia_app/config/constanst/environment.dart';
+import 'package:cinepedia_app/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+import '../domain/datasources/movies_datasource.dart';
+
+class MoviesdbDatasource extends MoviesDatacource {
+  final dio = Dio(BaseOptions(baseUrl: 'https://api.themoviedb.org/3', queryParameters: {
+    'api_key': Environment.movieDbKey,
+    'language': 'es-CO'
+  }));
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 0}) async {
+    final response = await dio.get('/movie/now_playing');
+    final List<Movie> movies = [];
+    
+    return movies;
+  }
+}

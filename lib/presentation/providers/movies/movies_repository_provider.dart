6b209df74fd86e100047cd6fc88e30af9968e366
AndresCamiloChavez <andres.cz.chavez@gmail.com>
2023-04-27 17:34:38 -0500

    Implementación de riverpod

---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,9 @@
+import 'package:cinepedia_app/infraestructure/datasources/moviedb_datasource.dart';
+import 'package:cinepedia_app/infraestructure/respositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+//Este repositorio es inmutable
+final movieRespositoryProvider = Provider((ref){ // es para que quedé inicalizado
+  return MovieRepositoryImpl(MoviesdbDatasource());
+});

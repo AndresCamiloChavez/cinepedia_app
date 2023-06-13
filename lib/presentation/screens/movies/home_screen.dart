3f147db7e0a3669a567ee461e76178532661c5ae
AndresCamiloChavez <andres.cz.chavez@gmail.com>
2023-06-13 16:36:41 -0500

    Implementación de swiper y de loading image

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cinepedia_app/config/constanst/environment.dart';
+import 'package:cinepedia_app/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -39,12 +39,11 @@ class HomeViewState extends ConsumerState<HomeView> {
     if (nowPlayingMovies.isEmpty) {
       return const Center(child: CircularProgressIndicator());
     }
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(title: Text(movie.title));
-      },
+    return Column(
+      children: [
+        const CustomAppbar(),
+        MoviesSlideShow(movies: nowPlayingMovies)
+      ],
     );
   }
 }
